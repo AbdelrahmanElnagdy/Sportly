2f67f23080fa322180dc688b60f54bb40f0520f5
SohaMohamed98 <48870254+SohaMohamed98@users.noreply.github.com>
2021-04-29 09:27:33 -0700

    check Network in favourite

--- Sportly/Views/Custom Views/FavouriteCollectionViewCell.swift
+++ Sportly/Views/Custom Views/FavouriteCollectionViewCell.swift
@@ -13,12 +13,16 @@ class FavouriteCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var favouriteName: UILabel!
     @IBOutlet weak var youtubeIcon: UIButton!
     
+    @IBOutlet weak var viewDelete: UIView!
+    
     var youTubeButton : (()-> Void)? = nil
     var deleteButton : (()-> Void)? = nil
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.containerView.collectionCellLayout()
         favouriteImage.circular()
+        viewDelete.roundCorners(corners: [.topRight, .bottomRight], radius: 35)
     }
     @IBAction func youtubeButton(_ sender: Any) {
         if let youTubeBtnIsClicked = youTubeButton {
