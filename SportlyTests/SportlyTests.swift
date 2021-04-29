c62debdf090714ebf43e06ee126ccbac9c0db11b
Abdelrahman Elnagdy <Abdelrahman.elnagdy.eng@gmail.com>
2021-04-29 22:57:50 +0200

    add unit testing scope

---
+++ SportlyTests/SportlyTests.swift
@@ -0,0 +1,22 @@
+//
+//  SportlyTests.swift
+//  SportlyTests
+//
+//  Created by Abdelrahman Elnagdy on 29/04/2021.
+//
+
+import XCTest
+
+class SportlyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+
+}
